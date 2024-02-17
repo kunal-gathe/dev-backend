6dd7709ff2de69c8b9b08a98817bfefd77a5369f
Kunal Gathe <kunalgathe121@gmail.com>
2024-02-17 13:07:59 +0530

    utils

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express, { urlencoded } from 'express';
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    Credentials: true
+}))
+app.use(express.json({limit: '16kb'}))
+app.use(urlencoded({extended: true}))
+app.use(express.static('public'))
+
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-
 import dotenv from 'dotenv'
-import connectDB from './db/index.js';
-
-dotenv.config({path: './env'})
+import connectDB from './db/index.js'
+import { app } from './app.js'
+dotenv.config({ path: './env' })
 
 connectDB()
+  .then(() => {
+    app.on('error', (err) => {
+      console.log('ERR', err)
+      throw err
+    })
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port ${process.env.PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.log('Mongo connection failed', err)
+  })
